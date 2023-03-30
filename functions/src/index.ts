0589849ee898b490df09ace051db8d8bedb4c3a0
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-03-30 16:21:27 +0900

    Targeted Notification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -48,7 +48,28 @@ export const onLikedCreated = functions.firestore
       .update({
         likes: admin.firestore.FieldValue.increment(1),
       });
-  });
+ const video = await (
+       await db.collection("videos").doc(videoId).get()
+     ).data();
+     if (video) {
+       const creatorUid = video.creatorUid;
+       const user = await (
+         await db.collection("users").doc(creatorUid).get()
+       ).data();
+       if (user) {
+         const token = user.token;
+         await admin.messaging().sendToDevice(token, {
+           data: {
+             screen: "123",
+           },
+           notification: {
+             title: "someone liked you video",
+             body: "Likes + 1 ! Congrats! 💖",
+           },
+         });
+       }
+     }
+   });
 
 export const onLikedRemoved = functions.firestore
   .document("likes/{likeId}")
